a011181abb78e5e7187b2c5fcb5e262627e57c37
Kshema Vargheese <kvargheese@meetingexpectations.com>
2024-09-04 21:39:11 -0400

    Added markdown to html converter

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MarkdownToHtmlController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/mhc', 'App\Http\Controllers\MarkdownToHtmlController@index');
+Route::post('/convert', 'App\Http\Controllers\MarkdownToHtmlController@convert');
